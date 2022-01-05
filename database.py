a84989206151887afd0a5b1237d9af50dea3a531
Jaasim <91386746+Jaasim2008@users.noreply.github.com>
2022-01-05 17:31:11 +0400

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-breakfast_foods = ['Eggs', 'Bread', 'Parota', 'Tamales', 'Waffles', 'French Toast', 'Cereal', 'Pancakes', 'Bacon']
-breakfast_prices = [2, 5, 3, 5, 2, 7, 1, 2, 9]
-lunch_foods = ['Rice', 'Fish', 'Chicken', 'Salad', 'Pasta']
-lunch_prices = [4, 6, 10, 4, 6]
-dinner_foods = ['Burger', 'Nuggets', 'Chips', 'Pizza', 'Tacos', 'Chimichangas']
-dinner_prices = [8, 7, 1, 15, 9, 69]
-dessert_foods = ['Strawberry Milkshake', 'Oreo Milkshake', 'Kitkat Milkshake', 'Lotus Milkshake',
-                 'Chocolate Milkshake']
-dessert_prices = [11, 16, 15, 20, 8]
