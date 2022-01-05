46429ef788ef67cd880ade02be6d4000b749dd17
Jaasim <91386746+Jaasim2008@users.noreply.github.com>
2022-01-05 14:53:27 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 breakfast_foods = ['Eggs', 'Bread', 'Parota', 'Tamales', 'Waffles', 'French Toast', 'Cereal', 'Pancakes', 'Bacon']
 breakfast_prices = [2, 5, 3, 5, 2, 7, 1, 2, 9]
 lunch_foods = ['Rice', 'Fish', 'Chicken', 'Salad', 'Pasta']
-lunch_prices = [4, 6, 10]
+lunch_prices = [4, 6, 10, 4, 6]
 dinner_foods = ['Burger', 'Nuggets', 'Chips', 'Pizza', 'Tacos', 'Chimichangas']
-dinner_prices = [8, 7, 1]
-dessert_foods = ['Chocolate Milk', 'Strawberry Milkshake', 'Oreo Milkshake', 'Kitkat Milkshake', 'Lotus Milkshake',
-                 'Chocolate Milkshake', 'Strawberry Milk']
+dinner_prices = [8, 7, 1, 15, 9, 69]
+dessert_foods = ['Strawberry Milkshake', 'Oreo Milkshake', 'Kitkat Milkshake', 'Lotus Milkshake',
+                 'Chocolate Milkshake']
+dessert_prices = [11, 16, 15, 20, 8]
